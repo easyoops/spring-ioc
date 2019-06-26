d13da9db925b0a40cbaa7531adf1bb0f6aa999e8
陈继伟 <jiwei.chen@lanmaoly.com>
2019-06-26 22:03:42 +0800

    start

---
+++ src/main/java/com/green/sioc/SiocApplication.java
@@ -0,0 +1,13 @@
+package com.green.sioc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiocApplication.class, args);
+	}
+
+}
